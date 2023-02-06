256da7639126ab7c8aeb2b4750486dac79d2e388
Aidan Hill <123518297+ASHill11@users.noreply.github.com>
2023-02-06 16:33:08 -0600

    Beginnings of request function
    Incomplete

--- Log Parser.py
+++ Log Parser.py
@@ -10,6 +10,15 @@
 # Then, the program parses the data and gives what the lab asks for
 
 
+# IMPORT MODULES HERE
+import urllib.request
+
+
+# WRITE FUNCTIONS HERE
+def retrieve(url):
+    requested = urllib.request.urlopen(url)
+
+
 # Welcome message
 print('Howdy! Welcome to this server log parser.')
 print('This program is designed to work with .txt log files from an Apache web server.')
@@ -31,3 +40,4 @@ while True:
     else:
         print('Invalid input detected, please try again')
 
+UserUrl = input('Please input source URL: ')
