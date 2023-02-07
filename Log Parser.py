431ba5b1bd41ab2abe6971f3aa92a385273053ac
Aidan Hill <123518297+ASHill11@users.noreply.github.com>
2023-02-07 14:43:06 -0600

    Myriad changes
    Moved url code near the end of the program to bring it in line with procedure. Also made some spelling changes

--- Log Parser.py
+++ Log Parser.py
@@ -21,31 +21,22 @@ def retrieve(url):
     requested = urllib.request.urlopen(url)
     # Debug line, returns result code, comment out for final release
     print("Result: " + str(requested.getcode()))
-    
-    Commented out for new code, outide the function its easier to take the data
+
+    Commented out for new code, outside the function its easier to take the data
 """
 
-Text_File = urlib.request.urlopen("https://s3.amazonaws.com/tcmg476/http_access_log")
-
-Log = ""
-
-for i in Text_File:
-    Log += (str(i) + '/n')
-    
-    
 
 
 # Welcome message
 print('Howdy! Welcome to this server log parser.')
 print('This program is designed to work with .txt log files from an Apache web server.')
 
-
 # Default cache location
 cache = r'C:\Program Files\Parser'
 skip = False
 # If the cache already exists in the default location it should skip this step
 if not os.path.exists(cache):
-    # This will skip cache clearing if used
+    # This will skip cache clearing if triggered
     skip = True
     # Cache is created at the file path set in the cache variable on line 31
     os.makedirs(cache)
@@ -68,10 +59,15 @@ while True:
     else:
         print('Invalid input detected, please try again')
 
-
 UserUrl = input('Please input source URL: ')
 retrieve(UserUrl)
 
+Text_File = urllib.request.urlopen("https%3A//s3.amazonaws.com/tcmg476/http_access_log")
+
+Log = ""
+
+for i in Text_File:
+    Log += (str(i) + '/n')
 
 """ 
 please set the file to be a big string 
@@ -86,7 +82,7 @@ lmk if there are any issues
 (432) 631 - 3606 
 -Miles
 """
-# to seperate the file into individual logs as a list
+# to separate the file into individual logs as a list
 # then take the first characters off and leave the date at the start
 Log = Log.split('/n')
 count = 0
@@ -96,40 +92,39 @@ for i in Log:
         if j == '[':
             break
         else:
-            spot += 1 
-    i = i[spot+4:]
-    Log[count] = i 
+            spot += 1
+    i = i[spot + 4:]
+    Log[count] = i
     count += 1
-   
 
 Total = count
 
-count = 0 
+count = 0
 
 for i in Log:
-     #print (i[0:3])
-     #print(i[4:8])
-     #right year for last 6 months?
+    # print (i[0:3])
+    # print(i[4:8])
+    # right year for last 6 months?
     if i[4:8] == "1995":
-         #right month?
+        # right month?
         if i[0:3] == 'Oct':
-             count += 1
+            count += 1
         elif i[0:3] == 'Sep':
-             count += 1
+            count += 1
         elif i[0:3] == 'Aug':
-             count += 1 
+            count += 1
         elif i[0:3] == 'Jul':
-             count += 1 
+            count += 1
         elif i[0:3] == 'Jun':
-             count += 1 
+            count += 1
         elif i[0:3] == 'May':
-             count += 1
-        #if not last 6 months dont count 
+            count += 1
+        # if not last 6 months dont count
         else:
-            continue  
+            continue
     else:
         continue
-     
-print()    
+
+print()
 print(count, "requests have been made in the last 6 months")
 print(Total, "requests are in the Log in total")
