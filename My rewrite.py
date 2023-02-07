52e2eca66bdbb717585eddc29fadd8ce4fdb77a9
Cailin Redden <123997299+cailinredden@users.noreply.github.com>
2023-02-07 13:35:18 -0600

    Add files via upload
    I adjusted some of the code. right now this creates the cache and prints the status of the connection and the url. This does not parse the data

--- My rewrite.py
+++ My rewrite.py
@@ -3,13 +3,54 @@ import os
 import glob
 
 
+
+
+# Separate cache creation
+# Default cache location
+cache = r'C:\Program Files'
+skip = False
+# If the cache already exists in the default location it should skip this step
+if not os.path.exists(cache):
+    # This will skip cache clearing if used
+    skip = True
+    # Cache is created at the file path set in the cache variable on line 31
+    os.makedirs(cache)
+
+# This variable deals with whether a user wants to clear the cache.
+# A while loop will enforce proper input
+while True:
+    if skip:
+        break
+    clear = input('Do you want to clear the cache? (y/n): ')
+    if clear == 'y':
+        files = glob.glob(cache)
+        for f in files:
+            os.remove(f)
+        break
+        # code to clear the cache
+    elif clear == 'n':
+        # If cache doesn't need to be cleared, then escape the loop
+        break
+    else:
+        print('Invalid input detected, please try again')
+
 #welcome message
 print('Howdy! Welcome to this server log parser.')
 print('This program is designed to work with .txt log files from an Apache web server.')
 url = input('Input your URL here: ')
+
+
+
 #inputs url and gives status
-rec_url = urllib.request.urlopen(url)
-print("Response Status: "+ str(rec_url.getcode()))
+def retrieve(url):
+    global rec_url
+    rec_url = urllib.request.urlopen(url)
+    print("Response Status: "+ str(rec_url.getcode()))
+    return rec_url
 #Save data in url as text file
+
+retrieve(url)
+print(rec_url)
+
 
 #divide text by line
